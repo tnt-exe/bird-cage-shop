0889b7480f3fd08f2fb02550e1f9b0cbf632adbb
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2023-10-16 05:33:12 +0700

    chore: remove admin env, add enum for cage img

--- BusinessObject/Enums/CageStatus.cs
+++ BusinessObject/Enums/CageStatus.cs
@@ -9,6 +9,13 @@ namespace BusinessObject.Enums
     public enum CageStatus
     {
         Unavailable = 0,
+        Available = 1,
+        Custom = 3
+    }
+
+    public enum CageImage
+    {
+        Deleted = 0,
         Available = 1
     }
 }
