49ac70ffb05918c1565e9ee6b9cf3d037f9d2779
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2023-10-17 04:54:39 +0700

    feat(cage component): implement create component list

--- Repository/Implement/CageComponentRepository.cs
+++ Repository/Implement/CageComponentRepository.cs
@@ -1,10 +1,20 @@
-﻿using DataTransferObject;
+﻿using AutoMapper;
+using BusinessObject.Models;
+using DataAccessObject;
+using DataTransferObject;
 using Repository.Interface;
 
 namespace Repository.Implement
 {
     public class CageComponentRepository : ICageComponentRepository
     {
+        private readonly IMapper _mapper;
+
+        public CageComponentRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
         public bool DeleteCageComponent(int cageComponentId)
         {
             throw new NotImplementedException();
@@ -15,9 +25,15 @@ namespace Repository.Implement
             throw new NotImplementedException();
         }
 
-        public bool InsertCageComponent(CageComponentDTO cageComponentDTO)
+        public bool InsertCageComponent(List<CageComponentDTO> cageComponentList)
         {
-            throw new NotImplementedException();
+            List<CageComponent> componentList = new List<CageComponent>();
+            foreach (var item in cageComponentList)
+            {
+                var component = _mapper.Map<CageComponent>(item);
+                componentList.Add(component);
+            }
+            return CageComponentDAO.SingletonInstance.InsertCageComponent(componentList);
         }
 
         public bool UpdateCageComponent(CageComponentDTO cageComponentDTO)
