39f67c56e2a45938eb2bbdd83ddd6277b06d68ed
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2023-11-03 17:41:00 +0700

    feat: add user pages and register

---
+++ BirdCageShopRazorPage/Pages/User/Details.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using DataTransferObject;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.Interface;
+
+namespace BirdCageShopRazorPage.Pages.User
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IUserRepository _context;
+
+        public DetailsModel(IUserRepository context)
+        {
+            _context = context;
+        }
+
+        public UserDTO User { get; set; } = default!;
+
+        public IActionResult OnGet(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = _context.GetUserById((int)id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+    }
+}
---
+++ BirdCageShopRazorPage/Pages/User/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using BusinessObject.Enums;
+using DataTransferObject;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.Interface;
+
+namespace BirdCageShopRazorPage.Pages.User
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IUserRepository _context;
+
+        public IndexModel(IUserRepository context)
+        {
+            _context = context;
+        }
+
+        public IList<UserDTO> User { get; set; } = default!;
+
+        public IActionResult OnGet()
+        {
+            User = _context.GetAllUsers()
+                .Where(user => user.Role == "Customer")
+                .ToList();
+            return Page();
+        }
+
+        public IActionResult OnGetChangeStatus(int? userId, int? currentStatus)
+        {
+            if (userId == null)
+            {
+                return NotFound();
+            }
+            
+            var newStatus = (int)((currentStatus == (int)UserStatus.Active) ?
+                UserStatus.Ban : UserStatus.Active);
+            _context.ChangeUserStatus((int)userId, newStatus);
+            return RedirectToPage();
+        }
+    }
+}
